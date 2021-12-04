6d278e34ee4ededb2891f00552316b526948544a
heerucan <iwpow0307@gmail.com>
2021-12-04 23:33:23 +0900

    [CHORE] 접근제한자 추가

--- Sources/HureeKit/HureeKit.swift
+++ Sources/HureeKit/HureeKit.swift
@@ -1,7 +1,7 @@
 public struct HureeKit {
     public private(set) var text = "Hello, World!"
     
-    var message = "HureeKim"
+    public var message = "HureeKim, 마다가스카 종강 축하!! 방어사주신다구요? ㄱㅅ"
 
     public init() {
     }
