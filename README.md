cb6ea82d53f1c6e8a18b7ff7bd66947791ec0565
heerucan <iwpow0307@gmail.com>
2021-12-04 23:26:52 +0900

    [ADD] HureeKit 탄생

---
+++ README.md
@@ -0,0 +1,3 @@
+# HureeKit
+
+A description of this package.
---
+++ Sources/HureeKit/HureeKit.swift
@@ -0,0 +1,8 @@
+public struct HureeKit {
+    public private(set) var text = "Hello, World!"
+    
+    var message = "HureeKim"
+
+    public init() {
+    }
+}
